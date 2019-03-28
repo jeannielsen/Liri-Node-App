cbfb1ef37c2c30f5f997d9f1fd36c5af3ea9840f
Nielsen <jeanielsen@comcast.net>
2019-03-27 23:28:34 -0700

    first push

---
+++ keys.js
@@ -0,0 +1,9 @@
+console.log('this is loaded');
+
+exports.spotify = {
+  id: process.env.SPOTIFY_ID,
+  secret: process.env.SPOTIFY_SECRET
+};
+
+
+// OMDb API: http://www.omdbapi.com/?i=tt3896198&apikey=c9543702
---
+++ liri.js
@@ -0,0 +1,102 @@
+// code to read and set any environment variables with the dotenv package
+require('dotenv').config();
+// code required to import keys.js file and store it in a variable
+var keys = require("./keys.js");
+// access key information
+var Spotify = require('node-spotify-api');
+var spotify = new Spotify(keys.spotify);
+var inquirer = require("inquirer");
+var axios = require("axios");
+var moment = require("moment");
+var command = process.argv[2];
+var choice = process.argv[3];
+
+
+// IF THE PROCESS.ARGV==="MOVIE-THIS"
+var getMovie = function (movieName) {
+  axios.get("http://www.omdbapi.com/?s=" + movieName + "&apikey=c9543702")
+    .then(function (response) {
+      console.log(response.data.Search[0]);
+      // console.log("Title: ", Title);
+      // console.log("Year: ", Year);
+      // console.log("IMBD Rating: ", imdbRating);
+      // console.log("Rotten Tomatoes: ", Rating);
+      // console.log("Country: ", Country);
+      // console.log("Language: ", Language);
+      // console.log("Plot: ", Plot);
+      // console.log("Actors: ", Actors);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+};
+
+// IF THE PROCESS.ARGV==="CONCERT-THIS"
+function getConcerts(artist) {
+  axios.get(`"https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"`)
+    .then(function (response) {
+      var data = response.data;
+      var text = `${linebreak}\nUpcoming concerts for ${artist}\n${linebreak}\n`
+      console.log(text);
+      logData(text);
+      for (var i = 0; i < 5; i++) {
+        var datetime = moment(data[i].datetime).format("MMM Do YYYY");
+        var concertData = `Venue: ${data[i].venue.name}\nLocation: ${data[i].venue.city}, ${data[i].venue.region} ${data[i].venue.country}\nDate: ${datetime}\n\n`;
+        console.log(concertData);
+        logData(concertData);
+      };
+      liriAgain();
+    })
+};
+
+var getArtistNames = function (artist) {
+  return artist.name;
+}
+
+var getMeSpotify = function (songName) {
+  spotify.search({ type: 'track', query: songName }, function (err, data) {
+    if (err) {
+      console.log('Error occurred: ' + err);
+      return;
+    };
+
+    var songs = data.tracks.items;
+    for (var i = 0; i < songs.length; i++) {
+      console.log(i);
+      console.log("artist(s): " + songs[i].artists.map(getArtistNames));
+      console.log("song name: " + songs[i].name);
+      console.log("preview song: " + songs[i].preview_url);
+      console.log("album: " + songs[i].album.name);
+      console.log("-------------------------------")
+    }
+  });
+}
+
+var askLiri = function () {
+  if (command === "movie-this") {
+    console.log("Searching for: " + choice)
+    getMovie(choice)
+  }
+
+
+
+
+
+
+
+
+
+  // IF THE PROCESS.ARGV==="SPOTIFY-THIS-SONG"
+  else if (command === "spotify-this-song") {
+    console.log("searchingForSong")
+    getMeSpotify(choice);
+  }
+  // IF THE PROCESS.ARGV==="CONCERT-THIS"
+  else if (command === "concert-this") {
+    console.log("Searching for: " + choice)
+    getConcerts(choice);
+  }
+}
+
+askLiri()
+
